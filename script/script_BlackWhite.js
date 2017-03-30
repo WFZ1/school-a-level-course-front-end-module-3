2fbe4da474c8be124df9982168fbb61e520db61e
Pasha Skumenko <Pasha Skumenko>
2017-03-31 01:22:03 +0300

    Expanded BlackWhite

--- script/script_BlackWhite.js
+++ script/script_BlackWhite.js
@@ -1,22 +1,30 @@
-$(document).ready(function() {
-	(function ($) {
-		$.fn.BlackAndWhite = function (feature) {
-			
-			if (typeof feature === 'string') {
-				switch (feature) {
-					case 'off': $(this).css('filter', 'none');
-						return;
-					case 'on': $(this).css('filter', 'grayscale(100%)');
-						return;
-					default: return;
-				}
+(function ($) {
+	$.fn.BlackAndWhite = function (feature) {
+		if (typeof feature === 'string') {
+			switch (feature) {
+				case 'off': $(this).css('filter', 'none');
+					return;
+				case 'on': $(this).css('filter', 'grayscale(100%)');
+					return;
+				default: return;
 			}
-
-			$(this).css('filter', 'grayscale(100%)');
 		}
-	}( jQuery ));
+		if (!isNaN(parseFloat(feature)) && isFinite(feature)) {
+			$(this).css('filter', 'grayscale(' + feature + '%)');
+			return;
+		}
+		$(this).css('filter', 'grayscale(100%)');
+	}
+}( jQuery ));
 
+$(document).ready(function() {
 	$('#bwWrapper').BlackAndWhite();
 	$('#bwWrapper').BlackAndWhite('off');
 	$('#bwWrapper').BlackAndWhite('on');
+	$('#bwWrapper').BlackAndWhite(0);
+	$('#bwWrapper').BlackAndWhite(20);
+	$('#bwWrapper').BlackAndWhite(40);
+	$('#bwWrapper').BlackAndWhite(60);
+	$('#bwWrapper').BlackAndWhite(80);
+	$('#bwWrapper').BlackAndWhite(100);
 });
